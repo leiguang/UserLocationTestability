3e3d2f62caaed55f46f60c989090a34096a8c4c9
Guang Lei <575403441@qq.com>
2019-08-05 21:44:29 +0800

    Initial Commit

---
+++ UserLocation-V1/UserLocation-V1Tests/UserLocation_V1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  UserLocation_V1Tests.swift
+//  UserLocation-V1Tests
+//
+//  Created by Guang Lei on 2019/8/5.
+//  Copyright © 2019 Guang Lei. All rights reserved.
+//
+
+import XCTest
+@testable import UserLocation_V1
+
+class UserLocation_V1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
